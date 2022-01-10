79938c0a15e3a7dcf5cbe7d796dff9bb0b3a756b
Mamadou AMADOU <doumstyle@gmail.com>
2022-01-10 16:39:16 +0100

    create step done

--- routes/artistsRoute.js
+++ routes/artistsRoute.js
@@ -1,24 +1,61 @@
 var express = require("express");
 var router = express.Router();
-const artistModel = require("../models/artists");
+const ArtistModel = require("../models/artists");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  console.log(res);
-  res.render("dashboard/artists/list.hbs");
+router.get("/", async function (req, res, next) {
+  res.render("dashboard/artists/list.hbs", {
+    artists:  await ArtistModel.find(),
+    css: ['artists'],
+  });
 });
 
 router.get("/create", (req, res, next) => {
   res.render("dashboard/artists/create.hbs");
 });
 
+
+
 router.post("/create", async (req, res, next) => {
   try {
-    await artistModel.create(req.body);
+    const inBand = req.body.isBand === 'on' ? true : false;
+    const artist = {
+      name: req.body.name,
+      isBand: inBand,
+      description: req.body.description
+    }
+    console.log('req.body whaaat? >>>', req.body);
+    await ArtistModel.create(artist);
     res.redirect("/dashboard/artists");
   } catch (err) {
     next(err);
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
