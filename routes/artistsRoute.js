f9b41531fa48c325b08e8902de14257701571bd4
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-01-10 17:51:11 +0100

    pour le pote de Josse

--- routes/artistsRoute.js
+++ routes/artistsRoute.js
@@ -5,8 +5,8 @@ const ArtistModel = require("../models/artists");
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
   res.render("dashboard/artists/list.hbs", {
-    artists:  await ArtistModel.find(),
-    css: ['artists'],
+    artists: await ArtistModel.find(),
+    css: ["artists"],
   });
 });
 
@@ -14,17 +14,15 @@ router.get("/create", (req, res, next) => {
   res.render("dashboard/artists/create.hbs");
 });
 
-
-
 router.post("/create", async (req, res, next) => {
   try {
-    const inBand = req.body.isBand === 'on' ? true : false;
+    const inBand = req.body.isBand === "on" ? true : false;
     const artist = {
       name: req.body.name,
       isBand: inBand,
-      description: req.body.description
-    }
-    console.log('req.body whaaat? >>>', req.body);
+      description: req.body.description,
+    };
+    console.log("req.body whaaat? >>>", req.body);
     await ArtistModel.create(artist);
     res.redirect("/dashboard/artists");
   } catch (err) {
@@ -32,30 +30,30 @@ router.post("/create", async (req, res, next) => {
   }
 });
 
+router.get("/:id/delete", async (req, res, next) => {
+  await ArtistModel.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/dashboard/artists");
+    })
+    .catch((e) => console.error(e));
+});
 
+router.get("/:id/update", async (req, res, next) => {
+  await ArtistModel.findById(req.params.id)
+    .then((artist) => {
+      console.log(artist)
+      res.render("dashboard/artists/update.hbs", { artist });
+    })
+    .catch((e) => console.error(e));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post("/:id/update", async (req, res, next) => {
+  await ArtistModel.findByIdAndUpdate(id, req.body, { new: true })
+    .then((editedArtist) => {
+      console.log(editedArtist);
+      res.redirect("/dashboard/artist");
+    })
+    .catch((e) => console.error(e));
+});
 
 module.exports = router;
